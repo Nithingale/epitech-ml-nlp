f1817ad652fd6ca88eed238bdad76c0a35f4d77f
Vleb <victor.leblanc@epitech.eu>
2021-03-21 14:58:23 +0100

    [+] Improve the parser modulability [+] ngram management

--- __main__.py
+++ __main__.py
@@ -3,13 +3,19 @@
 import nltk
 import sys
 
-from source.blogParser import parseBlogs
+from source.blogParser import BlogUser, parseBlogs
 
 # -------------------------------------------------------------------------------------------------------------------- #
+
+TRAINING_TEST_RATIO = 0.8
 
 if __name__ == '__main__':
     nltk.download('punkt')
     nltk.download('stopwords')
 
-    parseBlogs()
+    blogData: list = parseBlogs(blogCount = 100)
+    trainingTestLimit: int = int(len(blogData) * TRAINING_TEST_RATIO)
+    trainingData: list = blogData[:trainingTestLimit]
+    testData: list = blogData[trainingTestLimit:]
+
     sys.exit(0)
--- source/blogParser.py
+++ source/blogParser.py
@@ -15,17 +15,21 @@ class BlogUser(object):
         self.astro = astro
         self.data = data
 
-def parseBlogXML(blogPath: str) -> list:
-    tree = ET.parse(blogPath)
-    return nltk.word_tokenize(''.join(post.text for post in tree.getroot().findall('post')))
+    def ngram(self, n: int) -> list:
+        return [self.data[i:i + n] for i in range(len(self.data) - n + 1)]
 
-def parseBlogs(blogDir: str = 'blogs') -> list:
-    blogUserList = []
-    stopWords = set(nltk.corpus.stopwords.words('english'))
-    for blogFile in os.listdir(blogDir):
-        blogPath = f'{blogDir}/{blogFile}'
+def parseBlogs(blogDir: str = 'blogs', blogCount: int = -1, lowerWords: bool = True, removeStopWords: bool = True) -> list:
+    blogUserList: list = []
+    stopWords: list = nltk.corpus.stopwords.words('english')
+    for blogFile in os.listdir(blogDir)[:blogCount] if blogCount >= 0 else os.listdir(blogDir):
+        blogPath: str = f'{blogDir}/{blogFile}'
         try:
-            blogData = [word for word in parseBlogXML(blogPath) if word not in stopWords]
+            blogData: list = nltk.word_tokenize(''.join(post.text for post in ET.parse(blogPath).getroot().findall('post')))
+            if lowerWords:
+                for word in blogData:
+                    word.lower()
+            if removeStopWords:
+                blogData = [word for word in blogData if (word if lowerWords else word.lower()) not in stopWords]
             blogUserList.append(BlogUser(*blogFile.split('.')[:-1], blogData))
         except:
             continue
