dd490768c869eb78cbe64decddfc90b6af48a61a
Vleb <victor.leblanc@epitech.eu>
2021-03-21 15:47:08 +0100

    [+] Working ML

--- __main__.py
+++ __main__.py
@@ -3,7 +3,12 @@
 import nltk
 import sys
 
-from source.blogParser import BlogUser, parseBlogs
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import Perceptron
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+from source.blogParser import parseBlogs
 
 # -------------------------------------------------------------------------------------------------------------------- #
 
@@ -12,10 +17,31 @@ TRAINING_TEST_RATIO = 0.8
 if __name__ == '__main__':
     nltk.download('punkt')
     nltk.download('stopwords')
+    stopWords: set = set(nltk.corpus.stopwords.words('english'))
 
-    blogData: list = parseBlogs(blogCount = 100)
+    blogData: list = parseBlogs()
     trainingTestLimit: int = int(len(blogData) * TRAINING_TEST_RATIO)
     trainingData: list = blogData[:trainingTestLimit]
     testData: list = blogData[trainingTestLimit:]
+
+    vectorizer = TfidfVectorizer()
+    classifier = Perceptron()
+    pipeline = Pipeline([
+        ('vectorizer', vectorizer),
+        ('classifier', classifier)
+    ])
+
+    trainingX = [userData.data for userData in trainingData]
+    trainingY = [userData.gender for userData in trainingData]
+    pipeline.fit(trainingX, trainingY)
+
+    testX = [userData.data for userData in testData]
+    testY = [userData.gender for userData in testData]
+    testYPredicted = pipeline.predict(testX)
+
+    print('Accuracy:', accuracy_score(testY, testYPredicted))
+    print('Precision:', precision_score(testY, testYPredicted, average = 'micro'))
+    print('Recall:', recall_score(testY, testYPredicted, average = 'micro'))
+    print('F-score:', f1_score(testY, testYPredicted, average = 'micro'))
 
     sys.exit(0)
--- source/blogParser.py
+++ source/blogParser.py
@@ -2,34 +2,24 @@
 
 import os
 import xml.etree.ElementTree as ET
-import nltk
 
 # -------------------------------------------------------------------------------------------------------------------- #
 
 class BlogUser(object):
-    def __init__(self, id: str, gender: str, age: str, activity: str, astro: str, data: list):
-        self.id = int(id)
-        self.gender = gender
-        self.age = int(age)
-        self.activity = activity
-        self.astro = astro
-        self.data = data
+    def __init__(self, id: str, gender: str, age: str, activity: str, astro: str, data: str):
+        self.id: int = int(id)
+        self.gender: str = gender
+        self.age: int = int(age)
+        self.activity: str = activity
+        self.astro: str = astro
+        self.data: str = data
 
-    def ngram(self, n: int) -> list:
-        return [self.data[i:i + n] for i in range(len(self.data) - n + 1)]
-
-def parseBlogs(blogDir: str = 'blogs', blogCount: int = -1, lowerWords: bool = True, removeStopWords: bool = True) -> list:
+def parseBlogs(blogDir: str = 'blogs', blogCount: int = -1) -> list:
     blogUserList: list = []
-    stopWords: list = nltk.corpus.stopwords.words('english')
     for blogFile in os.listdir(blogDir)[:blogCount] if blogCount >= 0 else os.listdir(blogDir):
         blogPath: str = f'{blogDir}/{blogFile}'
         try:
-            blogData: list = nltk.word_tokenize(''.join(post.text for post in ET.parse(blogPath).getroot().findall('post')))
-            if lowerWords:
-                for word in blogData:
-                    word.lower()
-            if removeStopWords:
-                blogData = [word for word in blogData if (word if lowerWords else word.lower()) not in stopWords]
+            blogData: str = ''.join(post.text for post in ET.parse(blogPath).getroot().findall('post'))
             blogUserList.append(BlogUser(*blogFile.split('.')[:-1], blogData))
         except:
             continue
